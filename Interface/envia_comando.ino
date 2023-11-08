79c7e53c9d50b30637183a59a17f478010fa9427
Guilherme <guilhermeamn1@gmail.com>
2023-11-07 22:25:09 -0300

    Interface plotando sensor

--- Interface/envia_comando.ino
+++ Interface/envia_comando.ino
@@ -1,21 +1,35 @@
 #define LED_PIN 11
+#define SENSOR A5
 #define BAUDRATE 9600
 int brightness = 0;
+float light = 0.0;
 
 void setup() {
   pinMode(LED_PIN, OUTPUT);
+  pinMode(SENSOR, INPUT);
   Serial.begin(BAUDRATE);
 }
 
 void loop() {
-    if(Serial.available() > 0) {
-        brightness = Serial.parseInt();
-        Serial.println(brightness);
+    light = analogRead(SENSOR);
+    light = map(light, 0, 1023, 255, 0);
 
-        brightness = constrain(brightness, 0, 255);
+    /* Serial.print("light [%]: "); */
+    Serial.println(light / 255 * 100);
 
-        if(brightness != 0) analogWrite(LED_PIN, brightness);
+    if(light > 200) analogWrite(LED_PIN, HIGH);
+    else analogWrite(LED_PIN, LOW);
 
-        delay(500);
-    }
+    // if(Serial.available() > 0) {
+    //     brightness = Serial.parseInt();
+    //     Serial.println(brightness);
+
+    //     brightness = constrain(brightness, 0, 255);
+
+    //     if(brightness != 0) analogWrite(LED_PIN, brightness);
+
+    //     delay(500);
+    // }
+
+    delay(500);
 }
--- Interface/interface.py
+++ Interface/interface.py
@@ -1,9 +1,21 @@
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import serial
+from threading import Thread
 from tkinter import *
 import customtkinter
 
+# Defines
 arduino_port = '/dev/ttyACM0'
 baud_rate = 9600
+thread_delay = 500 # ms
+y_min = 0
+y_max = 100 # [%]
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("green")
+
+arduino = serial.Serial(arduino_port, baud_rate)
+sensor_readings = []
 
 def change_brightness():
     brightness = slider.get()
@@ -13,24 +25,63 @@ def change_brightness():
 def slider_handler(value):
     print(value)
 
+def read_sensor():
+    global sensor_readings
+    while True:
+        print("Reading sensor values")
+        try:
+            value = arduino.readline().strip().decode('utf-8')
+            print("value: ", value)
+            sensor_readings.append(value)
+        except ValueError:
+            print("ValueError")
+            pass
+
+def update_plot():
+    global y_min, y_max
+    ax.clear()
+    ax.plot(sensor_readings, color='green', linewidth=2, marker='o', markersize=5, label='Leitura do Sensor')
+    ax.set_xlabel('Tempo')
+    ax.set_ylabel('Sensor')
+    ax.set_title('Leitura do Sensor em Tempo Real')
+    ax.grid(True, linestyle='--', alpha=0.7)
+    ax.legend(loc='upper right')
+    canvas.draw()
+    root.after(thread_delay, update_plot)
+
 if __name__ == "__main__":
-    customtkinter.set_appearance_mode("dark")
-    customtkinter.set_default_color_theme("green")
+    # Thread para fazer leitura da serial do Arduino
+    read_thread = Thread(target=read_sensor)
+    read_thread.daemon = True
+    read_thread.start()
 
     root = customtkinter.CTk()
 
-    root.geometry('500x250')
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+    root.geometry(f"{screen_width}x{screen_height}")
     root.title("Interface Ensaio de Tração")
 
-    label = customtkinter.CTkLabel(master=root, text="Controle a luminosidade do LED")
-    label.place(relx=0.5, rely=0.25, anchor=CENTER)
+    frame = customtkinter.CTkFrame(master=root)
+    frame.pack(pady=20, padx=60, fill=BOTH, expand=True)
 
-    slider = customtkinter.CTkSlider(master=root, from_=0, to=255, number_of_steps=255, command=slider_handler)
-    slider.place(relx=0.5, rely=0.45, anchor=CENTER)
+    label = customtkinter.CTkLabel(master=frame, text="Controle a luminosidade do LED", justify=customtkinter.LEFT)
+    label.pack(pady=10, padx=10)
 
-    button = customtkinter.CTkButton(master=root, text='Enviar para o Arduino', command=change_brightness)
-    button.place(relx=0.5, rely=0.65, anchor=CENTER)
+    slider = customtkinter.CTkSlider(master=frame, command=slider_handler, from_=0, to=255, number_of_steps=255)
+    slider.pack(pady=10, padx=10)
+    slider.set(0.5)
 
-    arduino = serial.Serial(arduino_port, baud_rate)
+    button = customtkinter.CTkButton(master=frame, text='Enviar para o Arduino', command=change_brightness)
+    button.pack(pady=10, padx=10)
+
+    fig, ax = plt.subplots(figsize=(4, 8))
+    ax.set_ylim([y_min, y_max])
+    canvas = FigureCanvasTkAgg(fig, master=frame)
+    canvas.draw()
+    canvas.get_tk_widget().pack(fill=BOTH, expand=True)
+
+    root.after(thread_delay, update_plot)
 
     root.mainloop()
